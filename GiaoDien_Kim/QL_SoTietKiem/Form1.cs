0111c38a290a130781b92669ebe7220a36934afb
TrieuKim47 <trieuvankim2002@gmail.com>
2022-10-19 12:43:39 +0700

    thiet ke UI

---
+++ GiaoDien_Kim/QL_SoTietKiem/Form1.cs
@@ -0,0 +1,15 @@
+namespace QL_SoTietKiem
+{
+    public partial class frmMoSoTietKiem : Form
+    {
+        public frmMoSoTietKiem()
+        {
+            InitializeComponent();
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ GiaoDien_Kim/QL_SoTietKiem/Form2.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace QL_SoTietKiem
+{
+    partial class frmDS_SoTietKiem
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 6);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(315, 28);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.Text = "bnrkbnfkjgngb";
+            // 
+            // frmDS_SoTietKiem
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1242, 450);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "frmDS_SoTietKiem";
+            this.Text = "DANH SÁCH SỔ TIẾT KIỆM ";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ComboBox comboBox1;
+    }
+}
